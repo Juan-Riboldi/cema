b11171dcad4124b3e6c97fcac4b8e3457f67213d
TheEni <108473886+TheEni@users.noreply.github.com>
2022-12-02 18:29:25 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// integrantes
+const boton = document.getElementById("boton");
+boton.style.backgroundColor="#64a19d";
+boton.style.color="#fff";
+boton.style.border="none";
+boton.style.textAlign="center";
+boton.style.fontSize="30px";
+boton.style.borderRadius="7px";
+boton.onclick=function(){alert("Hola! somos: Juan Riboldi, Marcos Marto, Lara Camila Mena, Sebastian Rappeti, Franco Tejerina, Andrea Monasterio, Julian Diaz y Francisco Morti"
+);
+boton.style.backgroundColor="darkviolet";
+};
+
+// opinion
+const guardar = document.getElementById("guardar");
+guardar.onclick=function(){alert("Guardado Exitosamente. Gracias por tu opinion :D")}
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+
+  let description = document.getElementById('description').value;
+  console.log(description)
+
+  let task = {
+    title,
+    description
+  };
+
+  if(localStorage.getItem('tasks') === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  getTasks();
+  document.getElementById('formTask').reset();
+  e.preventDefault();
+}
+
+
